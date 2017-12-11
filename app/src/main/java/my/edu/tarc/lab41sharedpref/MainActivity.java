baaf420a996878c91855107c1e78e6610f42eee7
SeeKT <seekt@acd.tarc.edu.my>
2017-12-11 10:58:55 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/lab41sharedpref/MainActivity.java
@@ -0,0 +1,90 @@
+package my.edu.tarc.lab41sharedpref;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewName;
+    private ImageView imageViewProfile;
+    private int gender;
+    private String title;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        textViewName = (TextView) findViewById(R.id.textViewName);
+        imageViewProfile = (ImageView) findViewById(R.id.imageViewProfile);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void loadPref() {
+        SharedPreferences mySharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(this);
+
+        String my_edittext_name = mySharedPreferences.getString("display_name", getString(R.string.pref_default_display_name));
+        String gender = mySharedPreferences.getString("gender", "-1");
+
+        if(gender.equals("-1")){
+            title="";
+            imageViewProfile.setImageResource(R.drawable.profile);
+        }else if(gender.equals("1")){
+            title = "Mr.";
+            imageViewProfile.setImageResource(R.drawable.male);
+        }else {
+            title = "Ms.";
+            imageViewProfile.setImageResource(R.drawable.female);
+        }
+        textViewName.setText(title + " " + my_edittext_name);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadPref();
+    }
+}
